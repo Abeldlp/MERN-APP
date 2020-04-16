b644dba66778343d91657f396e3f1a7bed2ea48a
Abel de la Paz <abel45991690@gmail.com>
2020-04-16 20:40:04 +0200

    Add frontend routes and files

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Nav from "./components/Nav";
+import ExerciseList from "./components/ExerciseList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateCustomer from "./components/CreateCustomer";
+
 function App() {
   return (
     <Router>
       <Nav />
       <br />
-      <Route path="/" component={exerciseList} />
-      <Route path="/edit/:id" component={editExercise} />
-      <Route path="/create" component={createExercise} />
-      <Route path="/customer" component={createCustomer} />
+      <Route path="/" component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/customer" component={CreateCustomer} />
     </Router>
   );
 }
